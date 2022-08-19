6ece0600825c546b0c86722c89996806b18cfe63
J Quintero <jfqs24@gmail.com>
2022-08-18 20:32:55 -0500

    agregando segundo ejercicio

---
+++ estructurasSecuenciales/eSec2.cpp
@@ -0,0 +1,22 @@
+// programa para ingresar elradio de un circulo y reportar area 
+// y longitud de la circunferencia
+
+// area = pi * r^2
+// longitud = 2 * pi * r
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+  double radio, area, length;
+  const double pi = 3.141592653589793;
+
+   cout << "Digita el radio" << endl;
+   cin >> radio;
+
+   area = pi * pow (radio, 2);
+   length = 2 * pi * radio;
+
+   return 0;
+}
